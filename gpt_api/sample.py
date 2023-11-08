d3b285a12de162c00567cbfb4cbcd3d26f004e58
savicktso <savicktso@gmail.com>
2023-11-08 22:43:38 +0900

    delete key

--- gpt_api/sample.py
+++ gpt_api/sample.py
@@ -1,16 +1,27 @@
 import openai 
-openai.api_key = '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'
-messages = [ {"role": "system", "content": 
-			"You are a intelligent assistant."} ] 
-while True: 
-	message = input("User : ") 
-	if message: 
-		messages.append( 
-			{"role": "user", "content": message}, 
-		) 
-		chat = openai.ChatCompletion.create( 
-			model="gpt-3.5-turbo", messages=messages 
-		) 
-	reply = chat.choices[0].message.content 
-	print(f"ChatGPT: {reply}") 
-	messages.append({"role": "assistant", "content": reply}) 
+import argparse
+
+def main():
+    # Usage: python3 sample.py <api_key>	
+    parser = argparse.ArgumentParser(description="api key for gpt-3.5 turbo")
+    parser.add_argument("api_key", type=str)
+    args = parser.parse_args()
+
+    openai.api_key = args.api_key
+    messages = [ {"role": "system", "content": 
+                "You are a intelligent assistant."} ] 
+    while True: 
+        message = input("User : ") 
+        if message: 
+            messages.append( 
+                {"role": "user", "content": message}, 
+            ) 
+            chat = openai.ChatCompletion.create( 
+                model="gpt-3.5-turbo", messages=messages 
+            ) 
+        reply = chat.choices[0].message.content 
+        print(f"ChatGPT: {reply}") 
+        messages.append({"role": "assistant", "content": reply}) 
+
+if __name__ == "__main__":
+    main()
